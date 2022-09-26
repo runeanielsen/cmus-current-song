e95109eb4024b70a18e9d088843f962025f0f4ab
Rune Nielsen <runenielsen@runbox.com>
2022-09-26 22:41:15 +0200

    now handle stopped cmus player

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use std::{process::Command, str};
 
 #[derive(Debug, PartialEq)]
 struct Song {
-    status: String,
     artist: String,
     title: String,
     position: u32,
@@ -37,24 +36,22 @@ fn query_song_information() -> String {
         .to_owned()
 }
 
-fn get_song_field(fields: &[&str], field_name: &str) -> Option<String> {
+fn get_field(fields: &[&str], field_name: &str) -> Option<String> {
     fields
         .iter()
         .find(|x| x.contains(field_name))
         .map(|x| x.replace(field_name, "").trim_start().to_owned())
 }
 
-fn make_song(information: &str) -> Song {
-    let fields: Vec<&str> = information.split('\n').collect();
+fn make_song(fields: &[&str]) -> Song {
     Song {
-        status: get_song_field(&fields, "status").expect("Could not get status field."),
-        artist: get_song_field(&fields, "tag artist").expect("Could not get artist field."),
-        title: get_song_field(&fields, "tag title").expect("could not get title field."),
-        position: get_song_field(&fields, "position")
+        artist: get_field(fields, "tag artist").expect("Could not get artist field."),
+        title: get_field(fields, "tag title").expect("could not get title field."),
+        position: get_field(fields, "position")
             .expect("could not get position field.")
             .parse()
             .unwrap(),
-        duration: get_song_field(&fields, "duration")
+        duration: get_field(fields, "duration")
             .expect("could not get duration field.")
             .parse()
             .unwrap(),
@@ -63,10 +60,16 @@ fn make_song(information: &str) -> Song {
 
 fn main() {
     let information = query_song_information();
-    let song = make_song(&information);
-    if song.status == "playing" {
-        println!("{}", format_song(&song));
-    }
+    let fields: Vec<&str> = information.split('\n').collect();
+    match get_field(&fields, "status") {
+        Some(status) => {
+            if status == "playing" {
+                let song = make_song(&fields);
+                println!("{}", format_song(&song));
+            }
+        }
+        None => {}
+    };
 }
 
 #[cfg(test)]
@@ -76,7 +79,6 @@ mod tests {
     #[test]
     fn format_song_test() {
         let song = Song {
-            status: String::from("Playing"),
             artist: String::from("Snorri Hallgrimsson"),
             title: String::from("…og minning þín rís hægt (Peter Gregson Rework)"),
             duration: 222,
@@ -109,7 +111,7 @@ mod tests {
     }
 
     #[test]
-    fn get_song_field_test() {
+    fn get_field_test() {
         let fields = vec!(
             "status playing",
             "file /home/user/music/Snorri_Hallgrimsson-Orbit_Reworked/01-02-Peter_Gregson-og_minning_bi_n_ri_s_haegt-SMR.flac",
@@ -149,46 +151,46 @@ mod tests {
         ];
 
         for assertion in assertions {
-            assert_eq!(assertion.0, get_song_field(&fields, assertion.1).expect(""));
+            assert_eq!(assertion.0, get_field(&fields, assertion.1).expect(""));
         }
     }
 
     #[test]
     fn retrieve_tags_test() {
-        let text = "status playing
-                    file /home/user/music/Snorri_Hallgrimsson-Orbit_Reworked/01-02-Peter_Gregson-og_minning_bi_n_ri_s_haegt-SMR.flac
-                    duration 222
-                    position 136
-                    tag album Orbit Reworked
-                    tag title …og minning þín rís hægt (Peter Gregson Rework)
-                    tag tracknumber 2
-                    tag discnumber 1
-                    tag date 2018
-                    tag genre Électronique
-                    tag albumartist Snorri Hallgrimsson
-                    tag artist Snorri Hallgrimsson
-                    set aaa_mode artist
-                    set continue true
-                    set play_library true
-                    set play_sorted false
-                    set replaygain disabled
-                    set replaygain_limit true
-                    set replaygain_preamp 0.000000
-                    set repeat false
-                    set repeat_current false
-                    set shuffle off
-                    set softvol false
-                    set vol_left 100
-                    set vol_right 100";
+        let fields = vec!(
+            "status playing",
+            "file /home/user/music/Snorri_Hallgrimsson-Orbit_Reworked/01-02-Peter_Gregson-og_minning_bi_n_ri_s_haegt-SMR.flac",
+            "duration 222",
+            "position 136",
+            "tag album Orbit Reworked",
+            "tag title …og minning þín rís hægt (Peter Gregson Rework)",
+            "tag tracknumber 2",
+            "tag discnumber 1",
+            "tag date 2018",
+            "tag genre Électronique",
+            "tag albumartist Snorri Hallgrimsson",
+            "tag artist Snorri Hallgrimsson",
+            "set aaa_mode artist",
+            "set continue true",
+            "set play_library true",
+            "set play_sorted false",
+            "set replaygain disabled",
+            "set replaygain_limit true",
+            "set replaygain_preamp 0.000000",
+            "set repeat false",
+            "set repeat_current false",
+            "set shuffle off",
+            "set softvol false",
+            "set vol_left 100",
+            "set vol_right 100");
 
         let expected = Song {
             artist: String::from("Snorri Hallgrimsson"),
             title: String::from("…og minning þín rís hægt (Peter Gregson Rework)"),
-            status: String::from("playing"),
             duration: 222,
             position: 136,
         };
 
-        assert_eq!(expected, make_song(text));
+        assert_eq!(expected, make_song(&fields));
     }
 }
