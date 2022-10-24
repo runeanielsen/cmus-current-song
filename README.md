8dc695a5047a4e68b9d524d807ce50c8973ce10b
Tidux <1330562+tidux@users.noreply.github.com>
2022-10-24 11:15:35 -0700

    IRC client integration, starting with irssi (#1)

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Current Song
 
 Small program to display current song playing in cmus using cmus-remote.
+
+## Installation
+`cargo build --release && cargo install --path .`
+
+## IRC client integration
+Drop script from irc/`$clientname`/ where your client expects it.
---
+++ irc/irssi/cmus-np.pl
@@ -0,0 +1,42 @@
+use strict;
+use Irssi;
+
+sub np
+{
+    my($data,$server,$witem) = @_; #loads irssi env.
+    my $raw_cmus_current_song = `current-song`;
+    my $cmus_current_song = "Now Playing: $raw_cmus_current_song";
+    #prints to the channel
+    if ($witem && ($witem->{type} eq "CHANNEL" || $witem->{type} eq "QUERY")) 
+    {
+        $witem->command("MSG ".$witem->{name}." $cmus_current_song");
+    }
+    else 
+    {
+        Irssi::print("You're not in a channel.");
+    }
+}
+
+sub help
+{
+    print '
+cmus(1) now-playing script
+--------------------------
+
+Use /np to display the song currently playing in C*Mus.
+Use /nphelp to print this message.
+Install cmus-current-song and test it against your C*Mus process to make this work;
+no other weird dependencies.
+
+Enjoy!';
+}
+
+#now to bind this shit to actual irssi commands
+Irssi::command_bind np        => \&np;
+Irssi::command_bind 'nphelp' => \&help;
+
+#notify user on successful load
+Irssi::print "cmus-np.pl loaded - /np help for help";
+
+
+
