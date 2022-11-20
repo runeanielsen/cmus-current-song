c13fbff2035407d8c9c52a25650cedfaa7864e5e
Rune Nielsen <runenielsen@runbox.com>
2022-11-21 00:58:36 +0100

    now shows current song even if it is paused

--- src/cmus.rs
+++ src/cmus.rs
@@ -1,34 +1,73 @@
+use std::fmt::Display;
 use std::process::Command;
 use std::str;
 
-pub struct PlayInfo(Vec<String>);
+#[derive(Debug, PartialEq, Eq)]
+pub struct PlayInfo {
+    artist: String,
+    title: String,
+    position: u32,
+    duration: u32,
+}
 
-impl PlayInfo {
-    pub fn new(fields: &[&str]) -> PlayInfo {
-        PlayInfo(fields.iter().map(ToString::to_string).collect())
-    }
+impl Display for PlayInfo {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        fn format_time(t: u32) -> String {
+            format!("{:0>2}:{:0>2}", (t / 60), (t % 60))
+        }
 
-    pub fn get_field(&self, field_name: &str) -> Option<String> {
-        self.0
-            .iter()
-            .find(|x| x.contains(field_name))
-            .map(|x| x.replace(field_name, "").trim_start().to_owned())
+        write!(
+            f,
+            "{} : {} ({}/{})",
+            self.artist,
+            self.title,
+            format_time(self.position),
+            format_time(self.duration)
+        )
     }
 }
 
-pub fn query_current_song() -> PlayInfo {
+impl PlayInfo {
+    fn new(fields: &[&str]) -> PlayInfo {
+        PlayInfo {
+            artist: get_field_value(fields, "tag artist").expect("Could not get artist field."),
+            title: get_field_value(fields, "tag title").expect("could not get title field."),
+            position: get_field_value(fields, "position")
+                .expect("could not get position field.")
+                .parse()
+                .unwrap(),
+            duration: get_field_value(fields, "duration")
+                .expect("could not get duration field.")
+                .parse()
+                .unwrap(),
+        }
+    }
+}
+
+fn get_field_value(fields: &[&str], field_name: &str) -> Option<String> {
+    fields
+        .iter()
+        .find(|x| x.contains(field_name))
+        .map(|x| x.replace(field_name, "").trim_start().to_owned())
+}
+
+pub fn query_play_info() -> Option<PlayInfo> {
     let output = Command::new("cmus-remote")
         .arg("-Q")
         .current_dir("/bin")
         .output()
         .expect("ls command failed to start");
 
-    let fields: Vec<_> = str::from_utf8(&output.stdout)
-        .expect("Could not convert to UTF-8.")
-        .split('\n')
-        .collect();
+    if output.status.success() {
+        let fields: Vec<_> = str::from_utf8(&output.stdout)
+            .expect("Could not convert to UTF-8.")
+            .split('\n')
+            .collect();
 
-    PlayInfo::new(&fields)
+        Some(PlayInfo::new(&fields))
+    } else {
+        None
+    }
 }
 
 #[cfg(test)]
@@ -36,7 +75,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn get_field_test() {
+    fn make_new_play_info_test() {
         let fields = vec!(
             "status playing",
             "file /home/user/music/Snorri_Hallgrimsson-Orbit_Reworked/01-02-Peter_Gregson-og_minning_bi_n_ri_s_haegt-SMR.flac",
@@ -64,7 +103,46 @@ mod tests {
             "set vol_left 100",
             "set vol_right 100");
 
-        let play_info = PlayInfo::new(&fields);
+        let expected = PlayInfo {
+            artist: "Snorri Hallgrimsson".to_string(),
+            title: "…og minning þín rís hægt (Peter Gregson Rework)".to_string(),
+            position: 136,
+            duration: 222,
+        };
+
+        let result = PlayInfo::new(&fields);
+
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn get_field_for_on_field_name() {
+        let fields = vec!(
+            "status playing",
+            "file /home/user/music/Snorri_Hallgrimsson-Orbit_Reworked/01-02-Peter_Gregson-og_minning_bi_n_ri_s_haegt-SMR.flac",
+            "duration 222",
+            "position 136",
+            "tag album Orbit Reworked",
+            "tag title …og minning þín rís hægt (Peter Gregson Rework)",
+            "tag tracknumber 2",
+            "tag discnumber 1",
+            "tag date 2018",
+            "tag genre Électronique",
+            "tag albumartist Snorri Hallgrimsson",
+            "tag artist Snorri Hallgrimsson",
+            "set aaa_mode artist",
+            "set continue true",
+            "set play_library true",
+            "set play_sorted false",
+            "set replaygain disabled",
+            "set replaygain_limit true",
+            "set replaygain_preamp 0.000000",
+            "set repeat false",
+            "set repeat_current false",
+            "set shuffle off",
+            "set softvol false",
+            "set vol_left 100",
+            "set vol_right 100");
 
         let assertions = vec![
             ("playing", "status"),
@@ -78,7 +156,25 @@ mod tests {
         ];
 
         for assertion in assertions {
-            assert_eq!(assertion.0, play_info.get_field(assertion.1).expect(""));
+            assert_eq!(
+                assertion.0,
+                get_field_value(&fields, assertion.1).expect("")
+            );
         }
+    }
+
+    #[test]
+    fn format_song_is_displayed_correctly() {
+        let play_info = PlayInfo {
+            artist: String::from("Snorri Hallgrimsson"),
+            title: String::from("…og minning þín rís hægt (Peter Gregson Rework)"),
+            duration: 222,
+            position: 136,
+        };
+
+        assert_eq!(
+            "Snorri Hallgrimsson : …og minning þín rís hægt (Peter Gregson Rework) (02:16/03:42)",
+            format!("{}", play_info)
+        );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,9 @@
 #![warn(clippy::all, clippy::pedantic)]
 
 mod cmus;
-mod song;
-
-use cmus::{query_current_song, PlayInfo};
-
-use crate::song::Song;
-
-impl From<PlayInfo> for Song {
-    fn from(play_info: PlayInfo) -> Self {
-        Song::new(
-            play_info
-                .get_field("tag artist")
-                .expect("Could not get artist field."),
-            play_info
-                .get_field("tag title")
-                .expect("could not get title field."),
-            play_info
-                .get_field("position")
-                .expect("could not get position field.")
-                .parse()
-                .unwrap(),
-            play_info
-                .get_field("duration")
-                .expect("could not get duration field.")
-                .parse()
-                .unwrap(),
-        )
-    }
-}
 
 fn main() {
-    let info = query_current_song();
-
-    if let Some(status) = info.get_field("status") {
-        if status == "playing" {
-            println!("{}", Into::<Song>::into(info));
-        }
-    };
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn retrieve_tags_test() {
-        let play_info = PlayInfo::new(
-            &["status playing",
-              "file /home/user/music/Snorri_Hallgrimsson-Orbit_Reworked/01-02-Peter_Gregson-og_minning_bi_n_ri_s_haegt-SMR.flac",
-              "duration 222",
-              "position 136",
-              "tag album Orbit Reworked",
-              "tag title …og minning þín rís hægt (Peter Gregson Rework)",
-              "tag tracknumber 2",
-              "tag discnumber 1",
-              "tag date 2018",
-              "tag genre Électronique",
-              "tag albumartist Snorri Hallgrimsson",
-              "tag artist Snorri Hallgrimsson",
-              "set aaa_mode artist",
-              "set continue true",
-              "set play_library true",
-              "set play_sorted false",
-              "set replaygain disabled",
-              "set replaygain_limit true",
-              "set replaygain_preamp 0.000000",
-              "set repeat false",
-              "set repeat_current false",
-              "set shuffle off",
-              "set softvol false",
-              "set vol_left 100",
-              "set vol_right 100"]);
-
-        let expected = Song {
-            artist: String::from("Snorri Hallgrimsson"),
-            title: String::from("…og minning þín rís hægt (Peter Gregson Rework)"),
-            duration: 222,
-            position: 136,
-        };
-
-        assert_eq!(expected, Into::into(play_info));
+    if let Some(info) = cmus::query_play_info() {
+        println!("{}", info);
     }
 }
